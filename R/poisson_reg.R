b457177350caeec688023c018207b4a3c33e22cf
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2026-09-10 15:22:34 -0400

    oops - change 'penalty' to 'lambda' in new penalty_arg call

--- R/poisson_reg.R
+++ R/poisson_reg.R
@@ -93,7 +93,7 @@ translate.poisson_reg <- function(x, engine = x$engine, ...) {
   if (engine == "glmnet") {
     # See https://parsnip.tidymodels.org/reference/glmnet-details.html
     .check_glmnet_penalty_fit(x, engine)
-    x <- set_glmnet_penalty_path(x, "penalty")
+    x <- set_glmnet_penalty_path(x, "lambda")
     # Since the `fit` information is gone for the penalty, we need to have an
     # evaluated value for the parameter.
     x$args$penalty <- rlang::eval_tidy(x$args$penalty)
